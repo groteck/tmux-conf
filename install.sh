183f8247e60dd6ba1451283a761a2d0dafd529ba
Juan Fraire <groteck@gmail.com>
2015-06-27 18:21:08 +0000

    Add intallation script from github repository Move submodules and power line to root path Change tmux source requirements

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
-scriptPath=$(readlink -f `dirname $0`)
-echo $scriptPath
-git --git-dir $scriptPath submodule init
-git --git-dir $scriptPath submodule update
-ln -s $scriptPath/.tmux.conf ~/.tmux.conf
-ln -s $scriptPath/.tmux.config ~/.tmux.config
+cd ~ && \
+git clone --recursive git://github.com/groteck/tmux-conf.git .tmux && \
+ln -s .tmux/.tmux.conf ~/.tmux.conf
