70475bb7baed67ce9f8065540a138bcba559842f
Juan Fraire <groteck@gmail.com>
2015-06-27 19:07:28 +0200

    add git-dir argument to git commands into install.sh file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 scriptPath=$(readlink -f `dirname $0`)
 echo $scriptPath
-git submodule init
-git submodule update
+git --git-dir $scriptPath submodule init
+git --git-dir $scriptPath submodule update
 ln -s $scriptPath/.tmux.conf ~/.tmux.conf
 ln -s $scriptPath/.tmux.config ~/.tmux.config
